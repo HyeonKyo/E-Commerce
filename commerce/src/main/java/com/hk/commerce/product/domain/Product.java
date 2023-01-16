c07127ccca2cf2a3199f288bb5c7c3ce2bedbae1
김현교 <zkdlajt96@naver.com>
2023-01-16 23:11:20 +0900

    feat(product) : 초기 상품 도메인 엔티티 설계 및 간단한 기능 코드를 TDD로 작성

---
+++ commerce/src/main/java/com/hk/commerce/product/domain/Product.java
@@ -0,0 +1,63 @@
+package com.hk.commerce.product.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name = "product_category", joinColumns = @JoinColumn(name = "id"))
+    private Set<Long> categoryIds;
+
+    private String name;
+
+    private String description;
+
+    private long price;
+
+    private int stock;
+
+    private String image;
+
+    private ProductState state;
+
+    @Builder
+    public Product(String name, String description, long price, int stock, String image) {
+        this.name = name;
+        this.description = description;
+        setPrice(price);
+        this.stock = stock;
+        this.image = image;
+        this.state = ProductState.SELLING;
+    }
+
+    public void editInformation(String name, String description, long price, int stock, String imageURL) {
+        this.name = name;
+        this.description = description;
+        setPrice(price);
+        this.stock = stock;
+        this.image = imageURL;
+    }
+
+    public void stopSelling() {
+        this.state = ProductState.STOP_SELLING;
+    }
+
+    private void setPrice(long price) {
+        if (price <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+    }
+}
