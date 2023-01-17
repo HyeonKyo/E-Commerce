da7dd62c30dd7275f781a2a1266bfc08f401c87f
김현교 <zkdlajt96@naver.com>
2023-01-17 23:33:45 +0900

    feat(member) : Member 엔티티 생성

---
+++ commerce/src/main/java/com/hk/commerce/member/domain/Member.java
@@ -0,0 +1,69 @@
+package com.hk.commerce.member.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    private String phoneNumber;
+
+    private String role;
+
+    private boolean emailVerified;
+
+    private Member(String email, String password, String name,
+                  String address, String phoneNumber, String role) {
+        setEmail(email);
+        setPassword(password);
+        setName(name);
+        setAddress(address);
+        setPhoneNumber(phoneNumber);
+        setRole(role);
+    }
+
+    public static Member createMember(String email, String password, String name,
+                                      String address, String phoneNumber, String role) {
+        return new Member(email, password, name, address, phoneNumber, role);
+    }
+
+    private void setEmail(String email) {
+        this.email = email;
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+
+    private void setPassword(String password) {
+        this.password = password;
+    }
+
+    private void setAddress(String address) {
+        this.address = address;
+    }
+
+    private void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    private void setRole(String role) {
+        this.role = role;
+    }
+}
